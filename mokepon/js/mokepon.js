ab2b69b0589445999da7becc6a8da722cbd776f2
Santiago Zapata <santi@Santiagos-MacBook-Pro.local>
2023-02-25 16:15:22 -0300

    second commit

---
+++ mokepon/js/mokepon.js
@@ -0,0 +1,103 @@
+let ataqueJugador
+let ataqueEnemigo
+
+function iniciarJuego(){
+    //guardamos en una variable lo que almacene el boton con ese id
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    //especificamos cual es el evento que nos interesa usar y que va a pasar cuando eso suceda
+    botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById("boton-fuego")
+    botonFuego.addEventListener("click",ataqueFuego)
+
+    let botonAgua = document.getElementById("boton-agua")
+    botonAgua.addEventListener("click",ataqueAgua)
+
+    let botonTierra = document.getElementById("boton-tierra")
+    botonTierra.addEventListener("click",ataqueTierra)
+
+
+
+}
+
+//en esta funcion, evaluamos que input esta seleccionado
+function seleccionarMascotaJugador(){
+
+    let inputHipodoge=document.getElementById("hipodoge")
+    let inputCapipepo=document.getElementById("capipepo")
+    let inputRatigueya=document.getElementById("ratigueya")
+    let spanMascotaJugador=document.getElementById("mascota-jugador")
+
+    if (inputHipodoge.checked) {
+        spanMascotaJugador.innerHTML="Hipodogue"
+    } else if(inputCapipepo.checked) {
+        spanMascotaJugador.innerHTML="Capipepo"
+    } else if(inputRatigueya.checked){
+        spanMascotaJugador.innerHTML="Ratigueya"
+    }else{
+        alert("selecciona una mascota")
+    }
+
+    seleccionarMascotaEnemigo()
+
+}
+
+function seleccionarMascotaEnemigo(){
+    let mascotaAleatorio = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+
+    if(mascotaAleatorio==1){
+        spanMascotaEnemigo.innerHTML= "hipogoque"
+
+    }else if(mascotaAleatorio==2){
+        spanMascotaEnemigo.innerHTML= "capipepo"
+
+    }else {
+        spanMascotaEnemigo.innerHTML= "ratigueya"
+
+
+    }
+
+}
+
+function aleatorio(min,max){
+    return Math.floor(Math.random()*(max - min + 1)+ min)
+}
+
+function ataqueFuego(){
+    ataqueJugador="fuego"
+    ataqueAleatorioEnemigo()
+
+}
+function ataqueAgua(){
+    ataqueJugador="agua"
+    ataqueAleatorioEnemigo()
+
+}
+function ataqueTierra(){
+    ataqueJugador="tierra"
+    ataqueAleatorioEnemigo()
+
+}
+
+function ataqueAleatorioEnemigo(){
+    let ataqueAleatorio = aleatorio(1,3)
+
+    if(ataqueAleatorio==1){
+        ataqueEnemigo= "fuego"
+
+    }else if(ataqueAleatorio==2){
+        ataqueEnemigo= "agua"
+    }else{
+        ataqueEnemigo="tierra"
+
+    }
+    ataqueEnemigo= document.getElementById("ataque-enemigo")
+    ataqueEnemigo.innerHTML=ataqueEnemigo
+
+
+}
+
+//mediante este metodo del navegador, recien ejecutamos las funciones cuando el html haya cargado
+window.addEventListener("load",iniciarJuego)
+
